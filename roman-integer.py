1bd31130c568f4dd81060bf0c1ccc33d59e68996
omvss <omvaishno.0@gmail.com>
2025-07-23 16:49:15 +0530

    fixing

--- roman-integer.py
+++ roman-integer.py
@@ -1,1 +1,21 @@
-print("hello")
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}  
+        res = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
+
+def main():
+   
+    roman_numeral = input("Enter a Roman numeral:").upper()
+    solution = Solution()
+
+ 
+    result = solution.romanToInt(roman_numeral)
+    print(f"The integer value of {roman_numeral} is:{result}")
